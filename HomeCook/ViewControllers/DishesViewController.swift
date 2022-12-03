a11e0b4019c208fdf2d4f5ae5b25d9f713bb49e4
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-12-03 19:48:14 +0100

    add cuisine title on dishes screen

--- HomeCook/ViewControllers/DishesViewController.swift
+++ HomeCook/ViewControllers/DishesViewController.swift
@@ -12,5 +12,6 @@ class DishesViewController: UIViewController {
         for (button, dish) in zip(dishButtons, dishes) {
             button.setTitle(dish.title, for: .normal)
         }
+        title = dishes[0].cuisine.rawValue
     }
 }
