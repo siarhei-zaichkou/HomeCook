efd68b430f8c5d33a0d947414e592114d00eccbb
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-12-03 13:02:02 +0100

    fix price type

--- HomeCook/Models/Dish.swift
+++ HomeCook/Models/Dish.swift
@@ -2,8 +2,8 @@
 struct Dish {
     let title: String
     let ingredients: [Ingredient]
-    var price: Double {
-        var cost = 0.0
+    var price: Int {
+        var cost = 0
         ingredients.forEach { ingredient in cost += ingredient.price }
         return cost
     }
